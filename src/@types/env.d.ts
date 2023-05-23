3b3015108a3165963f60db25d25160607202ac3d
João Gabriel <joao@3035tech.com>
2023-05-22 23:55:07 -0300

    setup env variables

---
+++ src/@types/env.d.ts
@@ -0,0 +1,4 @@
+declare module "@env" {
+  export const ANDROID_CLIENTE_ID: string;
+  export const IOS_CLIENTE_ID: string;
+}
--- src/screens/sign-in.tsx
+++ src/screens/sign-in.tsx
@@ -1,9 +1,47 @@
-import { Box, HStack, Heading, Text, VStack } from "native-base";
-import { ImageBackground } from "react-native";
+import { HStack, Heading, Text, VStack } from "native-base";
+import * as WebBroser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { Alert, ImageBackground } from "react-native";
 import backgroundImage from "../assets/background.png";
 import { Button } from "../components/button";
+import { ANDROID_CLIENTE_ID, IOS_CLIENTE_ID } from "@env";
+import { useEffect, useState } from "react";
+
+WebBroser.maybeCompleteAuthSession();
 
 export const SignIn = () => {
+  const [isAuthentication, setIsAuthentication] = useState(false);
+
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENTE_ID,
+    iosClientId: IOS_CLIENTE_ID,
+    scopes: ["profile", "email"],
+  });
+
+  const handleGoogleSignIn = async () => {
+    setIsAuthentication(true);
+    const response = await googleSignIn();
+    if (response.type !== "success") return setIsAuthentication(false);
+  };
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response?.authentication?.idToken) {
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`
+        )
+          .then((response) => response.json())
+          .then(console.log);
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta Google."
+        );
+        setIsAuthentication(false);
+      }
+    }
+  }, [response]);
+
   return (
     <ImageBackground source={backgroundImage} style={{ flex: 1 }}>
       <VStack flex="1" alignItems="center" pt="40" p="16">
@@ -14,7 +52,9 @@ export const SignIn = () => {
           Gestão de uso de veículos
         </Text>
         <HStack>
-          <Button onPress={() => {}}>Entrar com Google</Button>
+          <Button onPress={handleGoogleSignIn} isLoading={isAuthentication}>
+            Entrar com Google
+          </Button>
         </HStack>
       </VStack>
     </ImageBackground>
