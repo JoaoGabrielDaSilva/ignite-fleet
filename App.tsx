6925abf30ca70de5da156d13535451a032fc94ac
João Gabriel <joao@3035tech.com>
2023-05-23 16:00:56 -0300

    fix: add app loader with correct structure and providers

--- App.tsx
+++ App.tsx
@@ -5,28 +5,30 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { StatusBar } from "expo-status-bar";
-import { Box } from "native-base";
 import { SignIn } from "./src/screens";
 import { Providers } from "./src/components/providers";
 import { UserProvider } from "@realm/react";
 import { Routes } from "./src/routes";
 import { RealmProvider } from "./src/libs/realm";
+import { Loading } from "./src/components/loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  if (!fontsLoaded) return null;
-
   return (
     <Providers>
-      <Box flex="1">
-        <StatusBar style="light" backgroundColor="transparent" translucent />
-        <UserProvider fallback={SignIn}>
-          <RealmProvider>
-            <Routes />
-          </RealmProvider>
-        </UserProvider>
-      </Box>
+      {!fontsLoaded ? (
+        <Loading />
+      ) : (
+        <>
+          <StatusBar style="light" backgroundColor="transparent" translucent />
+          <UserProvider fallback={SignIn}>
+            <RealmProvider>
+              <Routes />
+            </RealmProvider>
+          </UserProvider>
+        </>
+      )}
     </Providers>
   );
 }
