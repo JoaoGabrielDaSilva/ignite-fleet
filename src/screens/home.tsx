708ae27ce4187761288699cf61929271641c4c09
João Gabriel <joao@3035tech.com>
2023-05-23 00:59:27 -0300

    authentication flow and header component

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,5 +1,10 @@
 import { Box } from "native-base";
+import { Header } from "../components/header";
 
 export const Home = () => {
-  return <Box flex="1" bg="red.200"></Box>;
+  return (
+    <Box flex="1" bg="gray.800">
+      <Header />
+    </Box>
+  );
 };
